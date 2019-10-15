0af588dbd89726635a9fc92a82fd1a3caefff55f
Simon <simon.duff@gmail.com>
2019-10-15 13:37:23 +1100

    Python 2.x and 3.x support

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 #!/usr/local/bin/python
 
-import BaseHTTPServer
+try:
+    from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+except:
+    from http.server import BaseHTTPRequestHandler, HTTPServer
 
+# listen on port 1024 of the localhost
 addy = ('',1024)
 
 # A global data structure for access between HTTP server and main code
@@ -10,28 +14,28 @@ class Config(object):
 config = Config()
 
 # define the http handler
-class httpd_handler(BaseHTTPServer.BaseHTTPRequestHandler):
+class httpd_handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        print("In GET Handler for ",self)
-        self.wfile.write(self.__dict__)
-        self.wfile.write(config.__dict__)
+        print(("In GET Handler for ",self))
+        self.wfile.write(str(self.__dict__).encode())
+        self.wfile.write(str(config.__dict__).encode())
         return
 
     def do_POST(self):
-        print("In POST Handler for ",self)
-        self.wfile.write(self.__dict__)
-        self.wfile.write(config.__dict__)
+        print(("In POST Handler for ",self))
+        self.wfile.write(str(self.__dict__).encode())
+        self.wfile.write(str(config.__dict__).encode())
         config.httpd_running = False
         return
 
 # create http daemon
-httpd = BaseHTTPServer.HTTPServer(addy,httpd_handler)
+httpd = HTTPServer(addy,httpd_handler)
 httpd.timeout = 0.1
 
-print "Starting HTTP Server"
+print("Starting HTTP Server")
 config.httpd_running = True
 while config.httpd_running:
     # Check for any pending HTTP events
     httpd.handle_request()
     # Run the rest of the program
-print "Stopped HTTP Server"
+print("Stopped HTTP Server")
